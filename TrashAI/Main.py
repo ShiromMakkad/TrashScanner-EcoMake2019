16fc8ec54ecb7d0774ce298d0d0847fdca95852f
Shirom Makkad <shirmak11@gmail.com>
2019-10-19 16:38:06 -0400

    Basic Training Code

--- TrashAI/Main.py
+++ TrashAI/Main.py
@@ -1,21 +1,59 @@
 import os
 import random
+from fastai.vision import *
 
 def main():
-    subsets = ['train','test']
-    waste_types = ['cardboard','glass','metal','paper','plastic','trash']
-
-    training = os.listdir(os.getcwd() + "\\data\\training")
-
-    for i in training:
-        trashTypes = os.listdir(os.getcwd() + "\\data\\" + i)
-        for j in trashTypes:
-            print(j)
-
     tfms = get_transforms(do_flip=True,flip_vert=True)
-    data = ImageDataBunch.from_folder(Path(os.getcwd()),test="test",ds_tfms=tfms,bs=16)
+    path = Path(os.getcwd())/"data"
+    data = ImageDataBunch.from_folder(path,test="test",ds_tfms=tfms,bs=16)
 
     learn = create_cnn(data,models.resnet34,metrics=error_rate)
+    learn.model
+    learn.lr_find(start_lr=1e-6,end_lr=1e1)
+    learn.recorder.plot()
+    learn.fit_one_cycle(20,max_lr=5.13e-03)
+    preds = learn.get_preds(ds_type=DatasetType.Test)
+    print(preds[0].shape)
+    preds[0]
+    data.classes
+    max_idxs = np.asarray(np.argmax(preds[0],axis=1))
+    yhat = []
+    for max_idx in max_idxs:
+        yhat.append(data.classes[max_idx])
+    yhat
+    learn.data.test_ds[0][0]
+
+    y = []
+
+    ## convert POSIX paths to string first
+    for label_path in data.test_ds.items:
+        y.append(str(label_path))
+
+    ## then extract waste type from file path
+    pattern = re.compile("([a-z]+)[0-9]+")
+    for i in range(len(y)):
+        y[i] = pattern.search(y[i]).group(1)
+
+    print(yhat[0:5])
+    ## actual values
+    print(y[0:5])
+    learn.data.test_ds[0][0]
+
+
+    cm = confusion_matrix(y,yhat)
+    print(cm)
+    df_cm = pd.DataFrame(cm,waste_types,waste_types)
+    plt.figure(figsize=(10,8))
+    correct = 0
+    sns.heatmap(df_cm,annot=True,fmt="d",cmap="YlGnBu")
+
+    for r in range(len(cm)):
+        for c in range(len(cm)):
+            if (r==c):
+                correct += cm[r,c]
+
+    accuracy = correct/sum(sum(cm))
+    accuracy
 
 if __name__ == "__main__":
     main()
